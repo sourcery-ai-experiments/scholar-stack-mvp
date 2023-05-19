02dac3d4289e594617ff561462c3d765f8d8bf36
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-19 10:57:56 -0700

    intial: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["@/assets/css/tailwind.css"],
+
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/supabase"],
+});
