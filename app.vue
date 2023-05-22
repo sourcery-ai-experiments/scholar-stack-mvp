2a0f3f7592efeb86021b79ea22aefe20b95bbe03
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-22 16:11:59 -0700

    feat: add page transition

--- app.vue
+++ app.vue
@@ -8,6 +8,49 @@
   </NuxtLayout>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import "vue3-lottie/dist/style.css";
+
+useHead({
+  title: "Scholar Stack",
+  bodyAttrs: {
+    class: "test",
+  },
+  link: [
+    {
+      href: "/apple-touch-icon.png",
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+    },
+    {
+      href: "/favicon-16x16.png",
+      rel: "icon",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      href: "/favicon-32x32.png",
+      rel: "icon",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      href: "/site.webmanifest",
+      rel: "manifest",
+    },
+  ],
+  meta: [{ name: "description", content: "My amazing site." }],
+});
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
