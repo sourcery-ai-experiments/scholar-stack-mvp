07e6fd1714916d2cf055a177fa7f44b1c25e9ca7
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-25 21:53:35 -0700

    fix: update header

--- components/ui/UiHeader.vue
+++ components/ui/UiHeader.vue
@@ -1,24 +1,74 @@
 <template>
   <header
-    class="header left-0 top-0 z-50 w-full border-b border-slate-200 bg-slate-100 px-4 pb-3 pt-2 text-slate-800"
+    class="header left-0 top-0 z-50 w-full border-b border-slate-200 px-4 pb-3 pt-2 text-slate-800"
   >
-    <div class="flex items-center justify-start">
-      <div class="px-8">
-        <NuxtLink
-          to="/"
-          class="flex flex-row items-center justify-start space-x-2"
-        >
-          <img src="/logo/logo.svg" alt="Logo" class="w-10" />
-          <span class="text-xl font-bold text-blue-800">Scholar Stack</span>
-        </NuxtLink>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center justify-start">
+        <div class="px-8">
+          <NuxtLink
+            to="/"
+            class="flex flex-row items-center justify-start space-x-2"
+          >
+            <img src="/logo/logo.svg" alt="Logo" class="w-10" />
+            <span class="text-xl font-bold text-cyan-600">Scholar Stack</span>
+          </NuxtLink>
+        </div>
+        <div class="flex w-fit justify-start space-x-4">
+          <NuxtLink to="/" class="navigation-item"> Home </NuxtLink>
+          <NuxtLink to="/projects" class="navigation-item"> Projects </NuxtLink>
+          <NuxtLink to="/auth/confirm-email" class="navigation-item">
+            Confirm Email
+          </NuxtLink>
+        </div>
       </div>
-      <div class="flex w-fit justify-start space-x-4">
-        <NuxtLink to="/" class=""> Home </NuxtLink>
-        <NuxtLink to="/projects"> Projects </NuxtLink>
-        <NuxtLink to="/auth/confirm-email"> Confirm Email </NuxtLink>
-        <NuxtLink to="/auth/register"> Register </NuxtLink>
-        <div v-if="loggedIn" @click="logout">Logout</div>
-        <NuxtLink v-else to="/auth/login"> Login </NuxtLink>
+      <div class="flex flex-row space-x-8">
+        <n-space justify="space-between" align="center">
+          <n-input size="large" placeholder="Search">
+            <template #suffix>
+              <Icon name="ion:search-sharp" />
+            </template>
+          </n-input>
+
+          <div class="flex items-center justify-center space-x-2">
+            <nuxt-link v-if="!loggedIn" to="/auth/login">
+              <n-button type="primary" secondary> Login </n-button>
+            </nuxt-link>
+            <nuxt-link v-if="!loggedIn" to="/auth/register">
+              <n-button type="info" secondary> Sign Up </n-button>
+            </nuxt-link>
+
+            <n-popover
+              v-else
+              trigger="click"
+              content-style="padding: 8px; border-radius: 5px"
+              footer-style="padding: 8px; border-radius: 5px"
+            >
+              <template #trigger>
+                <n-avatar
+                  :size="48"
+                  :src="`https://api.dicebear.com/6.x/thumbs/svg?seed=${user?.email}`"
+                  class="hover:cursor-pointer hover:opacity-80"
+                />
+              </template>
+              <div class="flex flex-col">
+                <nuxt-link to="/profile" class="dropdown-item">
+                  <Icon name="mingcute:user-4-fill" size="20" />
+                  <span> Your profile </span>
+                </nuxt-link>
+                <nuxt-link to="/profile" class="dropdown-item">
+                  <Icon name="ic:baseline-settings" size="20" />
+                  <span> Settings </span>
+                </nuxt-link>
+              </div>
+              <template #footer>
+                <div class="dropdown-item" @click="logout">
+                  <Icon name="majesticons:logout" size="20" />
+                  <span> Logout </span>
+                </div>
+              </template>
+            </n-popover>
+          </div>
+        </n-space>
       </div>
     </div>
   </header>
@@ -35,3 +85,13 @@ const logout = async () => {
   window.location.href = "/";
 };
 </script>
+
+<style scoped>
+.navigation-item {
+  @apply font-semibold text-slate-500 transition-all hover:text-slate-800;
+}
+
+.dropdown-item {
+  @apply flex w-full cursor-pointer items-center justify-start space-x-2 px-2 py-1 text-base font-normal text-slate-500 transition-all hover:bg-slate-100 hover:text-slate-800;
+}
+</style>
