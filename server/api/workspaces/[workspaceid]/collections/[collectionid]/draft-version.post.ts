961d951e7b3e10aa728513c5a0b84c39106e9192
Sanjay Soundarajan <sanjay.16@live.com>
2023-11-08 22:53:29 -0800

    🚑 fix: update new-version api route for clones

--- server/api/workspaces/[workspaceid]/collections/[collectionid]/draft-version.post.ts
+++ server/api/workspaces/[workspaceid]/collections/[collectionid]/draft-version.post.ts
@@ -21,16 +21,13 @@ export default defineEventHandler(async (event) => {
   }
 
   // get the latest version of the collection
-  const version = await prisma.version.findMany({
+  const latestVersion = await prisma.version.findFirst({
     orderBy: { created: "desc" },
-    take: 1,
     where: { collection_id: collectionid },
   });
 
-  console.log(version);
-
   // if there is no version or the latest version is published, create a new version
-  if (version.length === 0 || version[0].published) {
+  if (latestVersion && latestVersion.published) {
     const draftVersion = await prisma.version.create({
       data: {
         name: "Draft",
@@ -40,7 +37,78 @@ export default defineEventHandler(async (event) => {
       },
     });
 
-    console.log(draftVersion);
+    // Get all the resources in the latest version
+    const originalResources = await prisma.resource.findMany({
+      where: {
+        Version: {
+          some: {
+            id: latestVersion.id,
+          },
+        },
+      },
+    });
+
+    // clone each resource into the staging table
+    for (const originalResource of originalResources) {
+      // clone the resource itself
+      const newStagingResource = await prisma.stagingResource.create({
+        data: {
+          title: originalResource.title,
+          action: "cloned",
+          back_link_id: originalResource.back_link_id, // todo: check if this is correct
+          description: originalResource.description,
+          icon: originalResource.icon,
+          orignal_resource_id: originalResource.id,
+          target: originalResource.target,
+          type: originalResource.type,
+        },
+      });
+
+      // clone the relations to the resource
+
+      // clone the external relations
+      const originalExternalRelations = await prisma.externalRelation.findMany({
+        where: {
+          source_id: originalResource.id,
+        },
+      });
+
+      for (const originalExternalRelation of originalExternalRelations) {
+        await prisma.stagingExternalRelation.create({
+          data: {
+            action: "cloned",
+            original_source_id: originalResource.id,
+            resource_type: originalExternalRelation.resource_type || null,
+            source_id: newStagingResource.id,
+            target: originalExternalRelation.target,
+            target_type: originalExternalRelation.target_type,
+            type: originalExternalRelation.type,
+          },
+        });
+      }
+
+      // clone the internal relations
+      const originalInternalRelations = await prisma.internalRelation.findMany({
+        where: {
+          source_id: originalResource.id,
+        },
+      });
+
+      for (const originalInternalRelation of originalInternalRelations) {
+        await prisma.stagingInternalRelation.create({
+          data: {
+            action: "cloned",
+            mirror: originalInternalRelation.mirror,
+            original_source_id: originalResource.id,
+            original_target_id: originalInternalRelation.target_id,
+            resource_type: originalInternalRelation.resource_type,
+            source_id: newStagingResource.id,
+            target_id: originalInternalRelation.target_id,
+            type: originalInternalRelation.type,
+          },
+        });
+      }
+    }
 
     return {
       statusCode: 201,
@@ -51,6 +119,6 @@ export default defineEventHandler(async (event) => {
   // if there is a draft version, return it
   return {
     statusCode: 200,
-    version: version[0],
+    version: latestVersion,
   };
 });
