24b9ff2915fbd98a78e7f40c8349ec537da10813
Sanjay Soundarajan <sanjay.16@live.com>
2023-11-16 14:57:30 -0800

    🥅 feat: update 404 page

--- plugins/Vue3Lottie.client.ts
+++ plugins/Vue3Lottie.client.ts
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-named-as-default
-import Vue3Lottie from "vue3-lottie";
+import { Vue3Lottie } from "vue3-lottie";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Vue3Lottie, { name: "Vue3Lottie" });
+  nuxtApp.vueApp.component("Vue3Lottie", Vue3Lottie);
 });
